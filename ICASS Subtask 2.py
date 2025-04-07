c130900784cf5b3eec3b1fa96ed926cdb9acc2a5
nkanyezi03 <nhlakaelom47@gmail.com>
2025-04-07 11:45:25 +0200

    Add files via upload

---
+++ ICASS Subtask 2.py
@@ -0,0 +1,11 @@
+product_name= input("Please enter the product name:")
+quantity_received= int(input("Please enter total quantity received:"))
+supplier_name= input("Please enter the Supplier's name:")
+product_cost= float 100.95 
+total_cost= float (quantity_received * product_cost)
+
+if total_cost > 500.00 :
+    print( "Large Order!")
+
+
+print("Product:",product_name"," "Quantity:", quantity_received"," "Supplier:",supplier_name"," "Total Cost:",total_cost)
