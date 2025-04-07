158a99359f9ea5b4b9eb2dc2cb1c9261a3a08dad
nkanyezi03 <nhlakaelom47@gmail.com>
2025-04-07 11:39:55 +0200

    Update ICASS L4.py

--- ICASS L4.py
+++ ICASS L4.py
@@ -6,19 +6,19 @@ def global  adding_stock():
 
 
 def global low_stock():
-    if current_stock > 10:
+    if current_stock < 10:
         print("THE STOCK IS LOW, PLEASE RE-ORDER!")
 
 
 def global customer_orders():
     place_order= int(input( in range(11)("Please enter the amount of quantities you want:")))
 
-    if place_order < 10:
+    if place_order > 10:
         print("You can only order maximun 10 units!!")
 
     stock_level= (current_stock -= place_order)
     
-    if stock_level > place_order:
+    if stock_level < place_order:
         print("There is not enough stock available!!")
 
 
